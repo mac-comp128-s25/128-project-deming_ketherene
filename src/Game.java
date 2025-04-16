3ef6c1225fbce3f782605946221b44c345804ad4
KathereneLugo <klugo@macalester.edu>
2025-04-16 11:27:02 -0500

    --

---
+++ src/Game.java
@@ -0,0 +1,8 @@
+import edu.macalester.graphics.CanvasWindow;
+public class Game {
+    private CanvasWindow canvas;
+
+    public Game() {
+        
+    }
+}
---
+++ src/Tile.java
@@ -0,0 +1,12 @@
+import edu.macalester.graphics.Rectangle;
+
+public class Tile extends Rectangle{
+
+    public Tile(double x, double y, double width, double height) {
+        super(x, y, width, height);
+        //TODO Auto-generated constructor stub
+    }
+
+
+
+}
