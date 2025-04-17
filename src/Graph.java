30231ce7fd550d71538e91c299c83478c2077c3e
wdm <dwang1@macalester.edu>
2025-04-17 10:28:19 -0500

    Update Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -11,18 +11,11 @@ public class Graph {
     private int E;
     private int[][] matrix;
 
-    /**
-     * Initializes an empty graph with {@code V} vertices and 0 edges.
-     * param V the number of vertices
-     *
-     * @param V number of vertices
-     * @throws IllegalArgumentException if {@code V < 0}
-     */
     public Graph(int V) {
         if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
         this.V = V;
         this.E = 0;
-        //Initialize the matrix and fill it with 0s.
+
         matrix = new int[V][V];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < i; j++) {
@@ -31,18 +24,6 @@ public class Graph {
         }
     }
 
-    /**
-     * Initializes a graph from the specified input stream.
-     * The format is the number of vertices <em>V</em>,
-     * followed by the number of edges <em>E</em>,
-     * followed by <em>E</em> pairs of vertices, with each entry separated by whitespace.
-     *
-     * @param in the input stream
-     * @throws IllegalArgumentException if {@code in} is {@code null}
-     * @throws IllegalArgumentException if the endpoints of any edge are not in prescribed range
-     * @throws IllegalArgumentException if the number of vertices or edges is negative
-     * @throws IllegalArgumentException if the input stream is in the wrong format
-     */
     public Graph(Scanner in) {
         if (in == null) throw new IllegalArgumentException("argument is null");
         try {
@@ -70,44 +51,20 @@ public class Graph {
         }
     }
 
-
-    /**
-     * Returns the number of vertices in this graph.
-     *
-     * @return the number of vertices in this graph
-     */
     public int V() {
         return V;
     }
 
-    /**
-     * Returns the number of edges in this graph.
-     *
-     * @return the number of edges in this graph
-     */
     public int E() {
         return E;
     }
 
-    // throw an IllegalArgumentException unless {@code 0 <= v < V}
     private void validateVertex(int v) {
         if (v < 0 || v >= V)
             throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
     }
 
-    /**
-     * Adds the undirected edge v-w to this graph.
-     *
-     * @param v one vertex in the edge
-     * @param w the other vertex in the edge
-     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
-     */
     public void addEdge(int v, int w) {
-        //Complete the addEdge method. Check if both vertices are on the graph.
-        // If so, increment the total number of edges
-        // Mark that w is adjacent to v in the matrix
-        // Mark that v is adjacent to w in the matrix
-
         validateVertex(v);
         validateVertex(w);
 
@@ -119,16 +76,7 @@ public class Graph {
 
     }
 
-
-    /**
-     * Returns the vertices adjacent to vertex {@code v}.
-     *
-     * @param v the vertex
-     * @return the vertices adjacent to vertex {@code v}, as an iterable
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
-     */
     public List<Integer> adj(int v) {
-        //Return the vertices adjacent to vertex v. Make sure that v exists
         validateVertex(v);
         List<Integer> adjacentVertices = new ArrayList<>();
 
@@ -140,15 +88,7 @@ public class Graph {
         return adjacentVertices;
     }
 
-    /**
-     * Returns the degree of vertex {@code v}.
-     *
-     * @param v the vertex
-     * @return the degree of vertex {@code v}
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
-     */
     public int degree(int v) {
-        //Return the degree of vertex v. Make sure that v exists
         int count = 0;
         for (int i = 0; i < matrix.length; i++) {
             if (i == v) {
@@ -163,13 +103,6 @@ public class Graph {
         return count;
     }
 
-
-    /**
-     * Returns a string representation of this graph.
-     *
-     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
-     * followed by the <em>V</em> adjacency lists
-     */
     public String toString() {
         StringBuilder s = new StringBuilder();
         s.append(V + " vertices, " + E + " edges " + NEWLINE);
@@ -183,17 +116,5 @@ public class Graph {
             s.append(NEWLINE);
         }
         return s.toString();
-    }
-
-    /**
-     * Unit tests the {@code Graph} data type.
-     *
-     * @param args the command-line arguments
-     */
-    public static void main(String[] args) {
-        Graph G = new Graph(3);
-        G.addEdge(1,2);
-        G.addEdge(0,2);
-        System.out.println(G);
     }
 }
