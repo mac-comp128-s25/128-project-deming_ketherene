2c3b766b6bb3249edee0b90c083f1ee2f3ff899a
KathereneLugo <klugo@macalester.edu>
2025-04-21 11:29:15 -0500

    --

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,15 @@
+
 import edu.macalester.graphics.CanvasWindow;
 
 public class Game {
     private CanvasWindow canvas;
+    private Tile tile;
 
     public Game() {
-        
+        this.canvas = new CanvasWindow("2048", 500, 500);
+    }
+
+    public static void main(String[] args) {
+        new Game();
     }
 }
