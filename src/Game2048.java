9faa8d45f1e9ced52110aaccce682377c5525c63
wdm <dwang1@macalester.edu>
2025-04-22 13:35:27 -0500

    fit

--- src/Game2048.java
+++ src/Game2048.java
@@ -1,18 +1,20 @@
 
 import edu.macalester.graphics.CanvasWindow;
 
-public class Game {
+public class Game2048 {
     private CanvasWindow canvas;
     private Graph graph;
 
-    public Game() {
+    public Game2048() {
         this.canvas = new CanvasWindow("2048", 500, 500);
         this.graph = new Graph(4);
         
 
     }
 
+    
+
     public static void main(String[] args) {
-        new Game();
+        new Game2048();
     }
 }
