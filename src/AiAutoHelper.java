ce8e9c6683c39cd131e24c7c627a2c245e1e9cf5
wdm <dwang1@macalester.edu>
2025-05-06 15:18:09 -0500

    Update AiAutoHelper.java

--- src/AiAutoHelper.java
+++ src/AiAutoHelper.java
@@ -1,8 +1,14 @@
 public class AiAutoHelper {
     private TileStorageGraph graph;
+    private Operations operations;
 
-    public AiAutoHelper(TileStorageGraph graph) {
+    public AiAutoHelper(TileStorageGraph graph, Operations operations) {
         this.graph = graph;
+        this.operations = operations;
+    }
+
+    public void runAi() {
+        
     }
     
 
