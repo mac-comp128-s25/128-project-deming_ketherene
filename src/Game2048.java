049e11abe116de1950327cd2aa1749f3b314ae77
KathereneLugo <klugo@macalester.edu>
2025-05-06 15:20:54 -0500

    win/lose progress

--- src/Game2048.java
+++ src/Game2048.java
@@ -15,29 +15,6 @@ public class Game2048 {
         graph = new TileStorageGraph(4);
         random = new Random();
         operations = new Operations();
-        run();
-    }
-
-    public void newTile() {
-        List<MatrixCoordinateStorage> storeList = new ArrayList<MatrixCoordinateStorage>();
-        Tile[][] tileMatrix = graph.getMatrix();
-        
-        for (int y = 0; y < tileMatrix.length; y++) {
-            for (int x = 0; x < tileMatrix[y].length; x++) {
-                if (!graph.hasTile(x, y)) {
-                    storeList.add(new MatrixCoordinateStorage(x, y));
-                }
-            }
-        }
-        
-        if (!storeList.isEmpty()) {
-            MatrixCoordinateStorage target = storeList.get(random.nextInt(storeList.size()));
-            graph.create(target.getX(), target.getY(), canvas);
-        }
-        
-    }
-
-    public void run() {
         canvas.onKeyDown(event -> {
             switch (event.getKey()) {
                 case LEFT_ARROW -> {
@@ -60,9 +37,86 @@ public class Game2048 {
                     newTile();
                 }
             }
+            winLose();
+
         });
+
+        // run();
+    }
+
+    public void newTile() {
+        List<MatrixCoordinateStorage> storeList = new ArrayList<MatrixCoordinateStorage>();
+        Tile[][] tileMatrix = graph.getMatrix();
+        
+        for (int y = 0; y < tileMatrix.length; y++) {
+            for (int x = 0; x < tileMatrix[y].length; x++) {
+                if (!graph.hasTile(x, y)) {
+                    storeList.add(new MatrixCoordinateStorage(x, y));
+                }
+            }
+        }
+        
+        if (!storeList.isEmpty()) {
+            MatrixCoordinateStorage target = storeList.get(random.nextInt(storeList.size()));
+            graph.create(target.getX(), target.getY(), canvas);
+        }
+        
+    }
+
+    public void run() {
+        // canvas.onKeyDown(event -> {
+        //     switch (event.getKey()) {
+        //         case LEFT_ARROW -> {
+        //             operations.moveLeft(graph.getMatrix());
+        //             newTile();
+        //         }
+
+        //         case RIGHT_ARROW -> {
+        //             operations.moveRight(graph.getMatrix());
+        //             newTile();
+        //         }
+
+        //         case UP_ARROW -> {
+        //             operations.moveUp(graph.getMatrix());
+        //             newTile();
+        //         }
+
+        //         case DOWN_ARROW -> {
+        //             operations.moveDown(graph.getMatrix());
+        //             newTile();
+        //         }
+        //     }
+        // });
         newTile();
         newTile();
+    }
+
+    private void winLose(){
+
+        if(graph.isFull()){
+           if(graph.openSpaces() > 0){
+            
+                run();
+            
+            //System.out.println("open spaces: " + graph.openSpaces());
+            //if (graph.openSpaces() == 0){
+            //    System.out.println("hello");
+           }
+            
+        }
+        if(graph.isFull())
+            System.out.println("Graph is full");
+        if(graph.isFull() && !operations.canMerge) {
+            System.out.println("game is over");
+            gameOver();
+        }
+
+    }
+
+    private void gameOver(){
+        canvas.removeAll();
+        canvas.closeWindow();
+        new Game2048();
     }
 
     public static void main(String[] args) {
